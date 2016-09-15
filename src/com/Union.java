64698c730bf98c9b97a17442950224710b0c06f9
SamuelRays <samrays@bk.ru>
2016-09-15 12:45:13 +0300

    Add bonus field to Union enum.

--- src/com/Union.java
+++ src/com/Union.java
@@ -1,14 +1,26 @@
 package com;
 
+import com.bonus.*;
+
 public enum Union {
-    SOUTH_EUROPE,
-    NORTH_AMERICA,
-    EAST_ASIA,
-    SOUTH_AMERICA,
-    MIDDLE_EUROPE,
-    WEST_EUROPE,
-    NORTH_EUROPE,
-    WEST_ASIA,
-    SMALL,
-    THREE_NATIONS_CUP;
+    SOUTH_EUROPE(CityBonus.class),
+    NORTH_AMERICA(FreeCityBonus.class),
+    EAST_ASIA(PaymentBonus.class),
+    SOUTH_AMERICA(FastBuildBonus.class),
+    MIDDLE_EUROPE(MoveChanceBonus.class),
+    WEST_EUROPE(MoreThrowsBonus.class),
+    NORTH_EUROPE(MoneyBonus.class),
+    WEST_ASIA(WonderBonus.class),
+    SMALL(PercentBonus.class),
+    THREE_NATIONS_CUP(OneCountryBonus.class);
+
+    private Class bonus;
+
+    Union(Class bonus) {
+        this.bonus = bonus;
+    }
+
+    public Class getBonus() {
+        return bonus;
+    }
 }
