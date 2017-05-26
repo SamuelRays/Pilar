5f154a2638a1b921f1d93707c25caf76d667ce61
SamuelRays <samrays@bk.ru>
2017-05-26 10:09:22 +0300

    New plan

--- Pilar_new/src/com/data/DataSource.java
+++ Pilar_new/src/com/data/DataSource.java
@@ -1,4 +1,27 @@
 package com.data;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class DataSource {
+    public static final long START_MONEY_AMOUNT = 750000;
+    public static final int DEFAULT_DESTROYED_CITIES = 1;
+    public static final int DEFAULT_FREE_CITIES = 1;
+    public static final double DEFAULT_FIRST_CITY_PRICE_RATIO = 1;
+    public static final double DEFAULT_SECOND_CITY_PRICE_RATIO = 1;
+    public static final double DEFAULT_THIRD_CITY_PRICE_RATIO = 1;
+    public static final double DEFAULT_PAYMENT_RATIO = 1;
+    public static final int DEFAULT_CITIES_PER_TURN_AVAILABLE = 1;
+    public static final int DEFAULT_AVAILABLE_THROWS = 1;
+    public static final long DEFAULT_SALARY = 5000;
+    public static final long DEFAULT_INCEPTION_BONUS = 5000;
+    public static final long DEFAULT_TAX = 5000;
+    public static final int DEFAULT_CITIES_WONDERS_RATIO = 3;
+    public static final double DEFAULT_PROFIT_DOWN = 0.9;
+    public static final double DEFAULT_PROFIT_UP = 1.1;
+    public static final double DEFAULT_ULTRA_PROFIT = 2;
+    public static final double DEFAULT_ONE_COUNTRY_BONUS_RATIO = 1;
+    public static final int DEFAULT_EVEN_MOVE_CHANCES = 0;
+    public static final int DEFAULT_MOVE_CHANCES = 0;
+    public static final Map<Integer, Long> BONUS_PRICES = new HashMap<>();
 }
--- Pilar_new/src/com/field/Union.java
+++ Pilar_new/src/com/field/Union.java
@@ -1,4 +1,101 @@
 package com.field;
 
+import com.game.Player;
+
+import java.util.*;
+
 public enum Union {
+    SOUTH_EUROPE(10000, 1700, Field.FRANCE, Field.SPAIN, Field.ITALY),
+    NORTH_AMERICA(16000, 1700, Field.USA, Field.CANADA),
+    EAST_ASIA(15000, 2200, Field.INDIA, Field.CHINA, Field.JAPAN),
+    SOUTH_AMERICA(5000, 600, Field.BRAZIL, Field.ARGENTINA, Field.COLUMBIA, Field.VENEZUELA),
+    MIDDLE_EUROPE(11000, 1800, Field.GERMANY, Field.AUSTRIA, Field.GREAT_BRITAIN),
+    WEST_EUROPE(12000, 1900, Field.NETHERLANDS, Field.DENMARK, Field.BELGIUM),
+    NORTH_EUROPE(13000, 2000, Field.NORWAY, Field.SWEDEN, Field.FINLAND),
+    WEST_ASIA(20000, 3500, Field.UAE, Field.KUWAIT, Field.QATAR),
+    SMALL(17000, 3000, Field.LUXEMBOURG, Field.VATICAN, Field.SINGAPORE, Field.MONACO),
+    THREE_NATIONS_CUP(14000, 2100, Field.AUSTRALIA, Field.NEW_ZEALAND, Field.SOUTH_AFRICA);
+
+    private Player owner;
+    private Map<Field, Player> countries = new HashMap<>();
+    private int price;
+    private int cityPrice;
+    private int visitCostPerCity;
+    private int cityAmount;
+    private int wonderAmount;
+    private int level;
+
+    Union(int price, int cityPrice, Field... countries) {
+        for (Field country : countries) {
+            this.countries.put(country, null);
+        }
+        this.price = price;
+        this.cityPrice = cityPrice;
+        this.visitCostPerCity = price / 10;
+    }
+
+    public static void setCountryOwner(Field countryField, Player player) {
+        if (!countryField.isCountry()) {
+            throw new IllegalArgumentException("Setting the owner to not a country field");
+        }
+        Union union = null;
+        for (Union i : Union.values()) {
+            if (i.countries.containsKey(countryField)) {
+                union = i;
+                union.countries.put(countryField, player);
+                union.checkOwner();
+            }
+        }
+        if (union == null) {
+            throw new RuntimeException("Country field has no union");
+        }
+    }
+
+    public void checkOwner() {
+        Player checkPlayer = null;
+        for (Map.Entry<Field, Player> i : countries.entrySet()) {
+            Player player = i.getValue();
+            if (player == null) {
+                owner = null;
+            }
+            if (checkPlayer == null) {
+                checkPlayer = player;
+            } else if (!checkPlayer.equals(player)) {
+                owner = null;
+            }
+        }
+        owner = checkPlayer;
+    }
+
+    public Player getOwner() {
+        return owner;
+    }
+
+    public Map<Field, Player> getCountries() {
+        return countries;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getCityPrice() {
+        return cityPrice;
+    }
+
+    public int getVisitCostPerCity() {
+        return visitCostPerCity;
+    }
+
+    public int getCityAmount() {
+        return cityAmount;
+    }
+
+    public int getWonderAmount() {
+        return wonderAmount;
+    }
+
+    public int getLevel() {
+        return level;
+    }
 }
